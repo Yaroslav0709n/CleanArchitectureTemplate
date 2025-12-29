b89ce0c6e6b50de39eb5fb96c530a1125528220d
Yaroslav <vizichkanich0709@gmail.com>
2025-12-29 21:57:44 +0200

    - start create mappers

---
+++ src/CleanArchitecture.Application/Mappers/AddressMapper.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Application.Addresses.Dto;
+using CleanArchitecture.Domain.Addresses;
+
+namespace CleanArchitecture.Application.Mappers;
+
+public static class AddressMapper
+{
+    public static AddressResponse ToAddressResponse(this Address address)
+    {
+        return new AddressResponse
+        {
+            City = address.City,
+            Street = address.Street,
+            HomeNumber = address.HomeNumber,
+            ApartmentNumber = address.ApartmentNumber,
+            ZipCode = address.ZipCode,
+            MailBox = address.MailBox
+        };
+    }
+}
---
+++ src/CleanArchitecture.Application/Mappers/OrganizationMapper.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Application.Organizations.Dto;
+using CleanArchitecture.Domain.Organizations;
+
+namespace CleanArchitecture.Application.Mappers;
+
+public static class OrganizationMapper
+{
+    public static OrganizationResponse ToOrganizationResponse(this Organization organization)
+    {
+        return new OrganizationResponse
+        {
+            Id = organization.Id,
+            Name = organization.Name,
+            Phone = organization.Phone,
+            Fax = organization.Fax,
+            Email = organization.Email,
+            Address = organization.Address?.ToAddressResponse()
+        };
+    }
+}
